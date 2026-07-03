4078e296e819ad7b81eba6d226418472d7950168
bestlife.maeda1977 <bestlife.maeda1977.gmail.com>
2026-07-03 12:42:35 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./style.scss";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const triggerTabList = [].slice.call(document.querySelectorAll('#pills-tab button'));
+document.addEventListener("DOMContentLoaded", () => {
+  const triggerTabList = [].slice.call(
+    document.querySelectorAll("#pills-tab button"),
+  );
   let activeIndex = 0;
 
   const showNextTab = () => {
     // 次のインデックスを計算
     activeIndex = (activeIndex + 1) % triggerTabList.length;
-    
+
     // BootstrapのTabインスタンスを取得して表示
     const tab = new bootstrap.Tab(triggerTabList[activeIndex]);
     tab.show();
@@ -17,4 +19,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // 4秒ごとに切り替え
   setInterval(showNextTab, 4000);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.classList.add("fade-out");
+
+    loader.addEventListener("transitionend", () => {
+      loader.style.display = "none";
+    });
+  }, 1000);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
